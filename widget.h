8b8651b97c23c40c547bb904a48e43a70f9b3289
Андрей Тарасенко <Lsa328@yandex.ru>
2024-02-27 15:35:42 +0700

    First Example programm
    Программа с учебника 2.7

---
+++ widget.h
@@ -0,0 +1,43 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QFrame>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QValidator>
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+protected:
+    QTextCodec *codec;
+    QFrame *frame; // рамка
+    QLabel *inputLabel; // метка ввода
+    QLineEdit *inputEdit; // строчный редактор ввода
+    QLabel *outputLabel; // метка вывода
+    QLineEdit *outputEdit; // строчный редактор вывода
+    QPushButton *nextButton; // кнопка Следующее
+    QPushButton *exitButton; // кнопка Выход
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+public slots:
+    void begin(); // метод начальной настройки интерфейса
+    void calc(); // метод реализации вычислений
+
+};
+
+
+class StrValidator:public QValidator // класс компонента проверки ввода
+{
+public:
+    StrValidator(QObject *parent):QValidator(parent){}
+    virtual State validate(QString &str,int &pos)const
+    {
+        return Acceptable; // метод всегда принимает вводимую строку
+    }
+};
+#endif // WIDGET_H
